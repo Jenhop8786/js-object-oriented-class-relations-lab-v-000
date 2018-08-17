8906bc096b3732fd29c2ea3ce3b5e6f5302fd3bc
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2018-08-17 16:41:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -36,7 +36,33 @@ class Driver {
        })
      }
 
-     
-
+     trips() {
+       return store.trips.filter(trip => {
+         return trip.passengerId == this.id;
+       })
+     }
   }
-}
+
+  let tripId = 0 
+
+  class Trip {
+    constructor(driver, passenger) {
+      this.driverId = driver.id;
+      this.passengerId = passenger.id;
+      this.id = tripId++;
+      store.trips.push(this);
+    }
+
+    driver() {
+      return store.drivers.find(driver => { 
+        return driver.id === this.driverId;
+      })
+    }
+
+    passenger() {
+      return store.passengers.find(passenger => {
+        return driver.id === this.passengerId;
+      })
+    }
+  }
+
